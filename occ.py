ee08a0e625217ffb74a5b4c864bb2b6a843b8b88
Manahal Tabassum <mtabassum@stuy.edu>
2017-09-17 18:06:26 -0400

    Finished random method

--- occ.py
+++ occ.py
@@ -1,4 +1,5 @@
 import csv
+import random
 occupations_dict = {}
 
 #with open('occupations.csv') as csvfile:
@@ -11,4 +12,25 @@ for row in reader:
 	k, v = row['Job Class'], float(row['Percentage'])
 	occupations_dict[k] = v
 	
-print occupations_dict
+#print occupations_dict
+
+def getRandOcc():
+	x = random.uniform(0,99.8)
+	for k in occupations_dict:
+		if (x - (occupations_dict.get(k)) < 0):
+			return k
+		else:
+			x = (x - (occupations_dict.get(k)))
+			
+
+print getRandOcc()
+print getRandOcc()
+print getRandOcc()
+print getRandOcc()
+print getRandOcc()
+print getRandOcc()
+print getRandOcc()
+print getRandOcc()
+print getRandOcc()
+print getRandOcc()
+
